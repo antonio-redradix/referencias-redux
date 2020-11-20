4fc29cd7da03c84b5a937f330aed92517f679b81
d-antesque <anon@anon.com>
2020-11-20 15:17:26 +0100

    added weather exercise

---
+++ weather/js/index.js
@@ -0,0 +1,7 @@
+//import "@babel/polyfill"
+
+import { main } from './main'
+import '../styles/main.scss'
+
+main()
+
---
+++ weather/js/main.js
@@ -0,0 +1,4 @@
+export function main(){
+    // code goes here
+    console.log('Hello world')
+}
