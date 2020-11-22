1bf2aaef79ee34c4a5a3f0375fea7d51bb3f2f44
d-antesque <anon@anon.com>
2020-11-22 19:00:08 +0100

    fix

--- soluciones/todos-persistent/src/components/filterTodos.jsx
+++ soluciones/todos-persistent/src/components/filterTodos.jsx
@@ -1,13 +1,15 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setFlag } from '../modules/UIFlags/actionCreators'
 
 export function FilterTodos(){
     const dispatch = useDispatch()
+    const taskFilter = useSelector(state => state.UIFlags.taskFilter)
 
     return (
         <div>
           <span> Filter </span>
-          <select selected="all" onChange={ev => dispatch(setFlag("taskFilter", ev.target.value))}>
+          <select selected="all" onChange={ev => dispatch(setFlag("taskFilter", ev.target.value))}
+                                   value={taskFilter}>
             <option value="all">All</option>
             <option value="done">Done</option>
             <option value="pending">Pending</option>
